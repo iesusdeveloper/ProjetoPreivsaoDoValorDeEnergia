e62345f89c5288b669c8322e9a5f0b90bea94265
Iesus Franco <76500125+iesusdeveloper@users.noreply.github.com>
2022-12-18 13:14:48 -0300

    Update formula.js

--- formula.js
+++ formula.js
@@ -6,12 +6,30 @@
 
 
 function calculo(){
-let leiat = parseFloat(document.getElementById("inputleiat").value);
-let leiant = parseFloat(document.getElementById("inputleiant").value);
-let trfkwh = parseFloat(document.getElementById("inputtrfkwh").value);
-let cilu = parseFloat(document.getElementById("inputcilu").value);
-let vbt = document.getElementById("inputvbt").value;
-let calculo = parseFloat(((leiat - leiant)*trfkwh + cilu+(vbt*(leiat-leiant))).toFixed(2));
+    let leiat = parseFloat(document.getElementById("inputleiat").value);
+    let leiant = parseFloat(document.getElementById("inputleiant").value);
+    let trfkwh = parseFloat(document.getElementById("inputtrfkwh").value);
+    let cilu = parseFloat(document.getElementById("inputcilu").value);
+    let vbt = document.getElementById("inputvbt").value;
+    calculo = parseFloat(((leiat - leiant)*trfkwh + cilu+(vbt*(leiat-leiant))).toFixed(2));
 
-document.getElementById("resultado").innerText = calculo
+    
+    // Função faz a verificação se a variável calculo há dados suficientes ou não para fazer o calculo
+function validCalculo(){
+
+    if (isNaN(calculo) == true){
+        return true;
+    }
+}
+
+    if (leiat <0 || leiant <0 || trfkwh <0 || cilu <0 || vbt <0){
+        document.getElementById("resultado").innerText ="Insira os valores para realizar o calculo"
+    }else if (validCalculo() ==true){ 
+        document.getElementById("resultado").innerText ="Insira os valores para realizar o calculo"
+    }else if (calculo <0){
+        document.getElementById("resultado").innerText ="O resultado não pode ser negativo"
+    }else{
+        document.getElementById("resultado").innerText = calculo
+
+    }
 }
