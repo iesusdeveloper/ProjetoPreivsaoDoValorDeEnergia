465d2cbeb157806573feea0a6ea5ee61a97b0f32
Iesus Franco <76500125+iesusdeveloper@users.noreply.github.com>
2023-01-11 10:43:42 -0300

    Update formula.js

--- formula.js
+++ formula.js
@@ -12,15 +12,22 @@ function calculo(){
     let cilu = parseFloat(document.getElementById("inputcilu").value);
     let vbt = document.getElementById("inputvbt").value;
     calculo = parseFloat(((leiat - leiant)*trfkwh + cilu+(vbt*(leiat-leiant))).toFixed(2));
-
+    
+    let button = document.querySelector('#resultado');
+    let i = 0;
+    button.addEventListener('click', function click(e){
+        i++;
+        button.removeEventListener('click',click);
+    });
     
     // Função faz a verificação se a variável calculo há dados suficientes ou não para fazer o calculo
-function validCalculo(){
-    if (isNaN(calculo) == true){
-        return true;
+    function validCalculo(){
+        
+        if (isNaN(calculo) == true){
+            return true;
+        }
     }
-}
-
+    
     if (leiat <0 || leiant <0 || trfkwh <0 || cilu <0 || vbt <0){
         document.getElementById("resultado").innerText ="Insira os valores para realizar o calculo"
     }else if (validCalculo() ==true){ 
@@ -28,7 +35,30 @@ function validCalculo(){
     }else if (calculo <0){
         document.getElementById("resultado").innerText ="O resultado não pode ser negativo"
     }else{
-        document.getElementById("resultado").innerText = calculo
-
+        document.getElementById("resultado").innerText ="Próxima fatura será de " + calculo.toLocaleString('pt-br',{style: 'currency', currency:'BRL'})
+        
     }
+    
+    let cep = parseInt(document.getElementById("inseriCep").value)
+    fetch(`https://viacep.com.br/ws/${cep}/json/`).then(response=>{
+        return response.json()
+    }).then(corpo=>{
+        
+        document.getElementById("cep").innerHTML="<strong>CEP: </strong>" + corpo.cep
+        document.getElementById("cidade").innerHTML="<strong>CIDADE: </strong>" + corpo.localidade
+        document.getElementById("uf").innerHTML="<strong>UF: </strong>" + corpo.uf
+        document.getElementById("bairro").innerHTML="<strong>BAIRRO: </strong>" + corpo.bairro
+        document.getElementById("complemento").innerHTML="<strong>COMPLEMENTO: </strong>" + corpo.complemento
+        document.getElementById("logradouro").innerHTML="<strong>RUA: </strong>" + corpo.logradouro
+        document.getElementById("ddd").innerHTML="<strong>DDD: </strong>" + corpo.ddd
+        
+        console.log(corpo)
+        console.log(typeof corpo.cep)
+        console.log(typeof corpo.localidade)
+        console.log(typeof corpo.localidade)
+        console.log(typeof corpo.bairro)
+        console.log(typeof corpo.complemento)
+        console.log(typeof corpo.logradouro)
+        console.log(typeof corpo.ddd)
+    })
 }
